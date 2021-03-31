31e0c072e13ecca06cb967d2bfa5a0387b29160c
Arpit Shrivastava <ashrivastava@ce.iitr.ac.in>
2021-03-31 20:35:07 +0530

    port

--- app.js
+++ app.js
@@ -49,6 +49,7 @@ app.post("/", function(req, res){
   });
 })
 
-app.listen(3000, function(){
-  console.log("server started on port 3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
 });
